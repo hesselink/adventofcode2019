566a0b2f8a7c9e08e5eb3c49ac7951953657b528
Erik Hesselink <ehesselink@palantir.com>
2019-12-19 17:17:53 +0100

    WIP

--- 16/Main.hs
+++ 16/Main.hs
@@ -1,3 +1,12 @@
+-- nth digit of the result doesn't depend on the first n digits of the
+-- input. Input is 650, *10000 is 65.000.000. But the first 8 digits
+-- are 59758034 so we can skip the first ~60M. This still leaves 5.241.966 digits.
+-- Currently this is quadratic in the number of digits, because we map
+-- 'digit' over the signal, and then digit zips and sums the signal
+-- again. We need some kind of dynamic programming approach perhaps?
+--
+-- The 5M inputs repeat every 650 items. The input patterns repeat
+-- every 4/8/12/16 etc items.
 import Data.Char (digitToInt)
 main :: IO ()
 main = do
